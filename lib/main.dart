1bc1475c992d58e2711b36bbc1da3e60d3bce8d2
nadymahmoud <asmaylsmt70@gmail.com>
2024-02-22 01:21:53 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DalelApp());
+}
+
+class DalelApp extends StatelessWidget {
+  const DalelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
